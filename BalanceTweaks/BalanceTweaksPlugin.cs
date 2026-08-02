632b69418b8f13192c8cafd248cd284c07bd7573
CoolLKK PS <PSKyle5926@outlook.com>
2026-08-03 03:43:04 +0800

    Add project files.

---
+++ BalanceTweaks/BalanceTweaksPlugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BalanceTweaksPlugin
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class BalanceTweaksPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Instance = this;
+            logger = base.Logger;
+            gameObject.hideFlags = HideFlags.HideAndDontSave;
+            harmony.PatchAll();
+            Logger.LogInfo("BalanceTweaks is loaded!");
+        }
+
+        public const string PLUGIN_GUID = "BalanceTweaks";
+        public const string PLUGIN_NAME = "BalanceTweaks";
+        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION_FULL = PLUGIN_VERSION + ".0";
+
+        Harmony harmony = new Harmony(PLUGIN_GUID);
+
+        public static ManualLogSource logger;
+        public static BalanceTweaksPlugin Instance;
+    }
+}
---
+++ BalanceTweaks/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion(BalanceTweaksPlugin.BalanceTweaksPlugin.PLUGIN_VERSION_FULL)]
+[assembly: AssemblyCompany(BalanceTweaksPlugin.BalanceTweaksPlugin.PLUGIN_NAME)]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyDescription("BalanceTweaks")]
+[assembly: AssemblyFileVersion(BalanceTweaksPlugin.BalanceTweaksPlugin.PLUGIN_VERSION_FULL)]
+[assembly: AssemblyInformationalVersion(BalanceTweaksPlugin.BalanceTweaksPlugin.PLUGIN_VERSION)]
+[assembly: AssemblyProduct(BalanceTweaksPlugin.BalanceTweaksPlugin.PLUGIN_NAME)]
+[assembly: AssemblyTitle(BalanceTweaksPlugin.BalanceTweaksPlugin.PLUGIN_NAME)]
