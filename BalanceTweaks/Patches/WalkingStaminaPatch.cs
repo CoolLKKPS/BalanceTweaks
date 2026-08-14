10e16490824cc6588ea7003b1b54d4d840b0aef6
CoolLKK PS <PSKyle5926@outlook.com>
2026-08-14 10:59:28 +0800

    Revert more

--- BalanceTweaks/Patches/WalkingStaminaPatch.cs
+++ BalanceTweaks/Patches/WalkingStaminaPatch.cs
@@ -21,6 +21,9 @@ namespace BalanceTweaksPlugin.Patches
             if (__instance.isSprinting)
                 return;
 
+            if (__instance.isCrouching)
+                return;
+
             if (__instance.isMovementHindered > 0)
                 return;
 
@@ -37,8 +40,7 @@ namespace BalanceTweaksPlugin.Patches
             float sprintTime = __instance.sprintTime;
             float carryWeight = __instance.carryWeight;
 
-            float walkDrainMultiplier = __instance.isCrouching ? 0f : 0.05f;
-            // float walkDrainMultiplier = 0f;
+            float walkDrainMultiplier = 0.25f;
 
             float vanillaRegenAmount = deltaTime / (sprintTime + 9f) * drunknessMultiplier;
             float drainAmount = deltaTime / sprintTime * carryWeight * drunknessMultiplier * walkDrainMultiplier;
